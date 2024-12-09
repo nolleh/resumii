98cd3b3fb64e67a1c5871ed54ab47a9613403340
nolleh <nolleh7707@gmail.com>
2024-12-09 15:04:20 +0900

    style: format

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,17 @@ import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
+	plugins: [sveltekit()],
 
-  test: {
-    include: ['src/**/*.{test,spec}.{js,ts}']
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler' // or "modern"
-        // includePaths: ['src/**/*', 'src/lib/styles/**/*']
-      }
-    }
-  }
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: 'modern-compiler' // or "modern"
+				// includePaths: ['src/**/*', 'src/lib/styles/**/*']
+			}
+		}
+	}
 });
